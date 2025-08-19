b5f761b11f4f32a0959d7ec75a7240504a50a5de
chris <chris@buildlackey.com>
2025-08-18 21:19:44 -0700

    had Path issue with flake..  fixed by making it a non relative absolute path.  not sure why it failed but in any event this fixed it.

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-    insynch.url = "path:./flakes/insynch";
+    insynch.url = "path:/home/chris/nixos-config/flakes/insynch";
   };
 
   outputs = { self, nixpkgs, home-manager, insynch, ... }: {
